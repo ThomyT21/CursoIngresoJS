2393e5f88b459c463b2b39995016768214e35b16
ThomyT21 <52494945+ThomyT21@users.noreply.github.com>
2019-07-18 12:28:28 -0300

    Add files via upload
    Ejercicios While

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,7 +3,16 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
 
+	while(contador < 5) {
+
+	numero = parseInt(prompt("Ingrese un Número"));
+	
+	acumulador = parseInt(acumulador + numero);
+
+	contador++;
+	}
 
 
 document.getElementById('suma').value=acumulador;
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,7 +3,22 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var numero;
+	var seguir;
+
+while (seguir =='s') {
+
+	numero = parseInt(prompt("ingrese un numero"));
+	while (isNaN(numero)) {
+		alert("chinwenwencha eso no es un numero");
+		numero = parseInt(prompt("ingrese un numero: "));
+	}
+
+	acumulador = acumulador + numero;
+	contador++;
+	seguir = prompt("Quiere ingresar otro numero?");
+	
+}
 
 
 document.getElementById('suma').value=acumulador;
